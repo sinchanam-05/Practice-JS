9c8ec86322249e38ad1295cf4d9bdc4c648bccba
sinchanam-05 <sinchanam0522@gmail.com>
2025-05-03 07:59:35 +0530

    morning commit

--- accordion/script.js
+++ accordion/script.js
@@ -1,8 +1,8 @@
 document.querySelectorAll(".accordion").forEach((header) => {
   header.addEventListener("click", () => {
     const open = document.querySelector('h2[aria-expanded="true"]');
-    if (open && open !== header) toggle(open); // close previous
-    toggle(header); // toggle current
+    if (open && open !== header) toggle(open);
+    toggle(header);
   });
 });
 
